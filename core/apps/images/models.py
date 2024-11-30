6669289568af5b022dd54c6dbc675ff49dd26849
Ivan Novozhilov <petyk98@gmail.com>
2024-11-30 13:19:36 +0700

    Добавил модель-миксин с датами обновления и создания записи; Добавил модель данных Images.

--- core/apps/images/models.py
+++ core/apps/images/models.py
@@ -1,4 +1,25 @@
 from django.db import models
 
+from core.apps.common.models import TimedBaseModel
+from core.apps.images.entities import ImageEntity
 
-# Create your models here.
+
+class Images(TimedBaseModel):
+    encoded_image = models.TextField()
+    description = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.description
+
+    def to_entity(self) -> ImageEntity:
+        return ImageEntity(
+            id=self.id,
+            created_at=self.created_at,
+            updated_at=self.updated_at,
+            encoded_image=self.encoded_image,
+            description=self.description,
+        )
+
+    class Meta:
+        verbose_name = "Image"
+        verbose_name_plural = "Images"
