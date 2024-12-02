4e1edc58f20633e686e292f619e612420b2d3e50
Ivan Novozhilov <petyk98@gmail.com>
2024-12-02 08:42:39 +0700

    Добавил ORMImageService; Рефакторинг BaseImageEntity, переименовал метод, добавил аргументы в методы, сделал методы статическими.

--- core/apps/images/services.py
+++ core/apps/images/services.py
@@ -2,17 +2,45 @@ from abc import ABC, abstractmethod
 from typing import Iterable
 
 from core.apps.images.entities import ImageEntity
+from core.apps.images.exceptions import ImageNotFound
+from core.apps.images.models import ORMImages
 
 
 class BaseImagesService(ABC):
+    @staticmethod
     @abstractmethod
-    def get_image_list(self) -> Iterable[ImageEntity]:
+    def get_image_list() -> Iterable[ImageEntity]:
         raise NotImplementedError
 
+    @staticmethod
     @abstractmethod
-    def add_image(self) -> None:
+    def add_image(image: ImageEntity) -> None:
         raise NotImplementedError
 
+    @staticmethod
     @abstractmethod
-    def remove_image(self) -> None:
+    def remove_image_by_id(image_id: int) -> None:  # noqa
         raise NotImplementedError
+
+
+class ORMImagesService(ABC):
+    @staticmethod
+    def get_image_list() -> Iterable[ImageEntity]:
+        query_set = ORMImages.objects.all()
+        return [image.to_entity() for image in query_set]
+
+    @staticmethod
+    def add_image(image: ImageEntity) -> None:
+        ORMImages.objects.create(
+            encoded_image=image.encoded_image,
+            description=image.description,
+        )
+
+    @staticmethod
+    def remove_image_by_id(image_id: int) -> None:  # noqa
+        try:
+            image_to_remove = ORMImages.objects.get(id=image_id)
+        except ORMImages.DoesNotExist:
+            raise ImageNotFound(image_id=image_id)
+
+        image_to_remove.delete()
