bcf3144eacd1a21e1e720d691e98ab63c46061d5
Ivan Novozhilov <petyk98@gmail.com>
2024-12-02 19:56:23 +0700

    Добавил исключения доменной области.

--- core/apps/images/exceptions.py
+++ core/apps/images/exceptions.py
@@ -4,9 +4,27 @@ from core.apps.common.exceptions import ApplicationException
 
 
 @dataclass(eq=False)
-class ImageNotFound(ApplicationException):
+class ImageNotFoundException(ApplicationException):
     image_id: int
 
     @property
     def message(self):
         return "Image not found"
+
+
+@dataclass(eq=False)
+class ValidateImageException(ApplicationException):
+    base64_payload: str
+
+    @property
+    def message(self):
+        return "Validate image encoding exception occurred"
+
+
+@dataclass(eq=False)
+class ValidateBase64PayloadException(ValidateImageException):
+    base64_payload: str
+
+    @property
+    def message(self):
+        return "Decode base64 image payload exception occurred"
