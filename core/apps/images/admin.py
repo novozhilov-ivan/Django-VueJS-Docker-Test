3e3d9378de74094a202ee5117cb1b160a5b4dc37
Ivan Novozhilov <petyk98@gmail.com>
2024-12-02 19:53:00 +0700

    Изменил поля в модели ORM. Добавил новые поля в админку. Создал миграции.

--- core/apps/images/admin.py
+++ core/apps/images/admin.py
@@ -5,4 +5,11 @@ from core.apps.images.models import ORMImages
 
 @admin.register(ORMImages)
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ("id", "encoded_image", "description", "created_at", "updated_at")
+    list_display = (
+        "id",
+        "base64_payload",
+        "extension",
+        "description",
+        "created_at",
+        "updated_at",
+    )
--- core/apps/images/models.py
+++ core/apps/images/models.py
@@ -2,11 +2,17 @@ from django.db import models
 
 from core.apps.common.models import TimedBaseModel
 from core.apps.images.entities import ImageEntity
+from core.apps.images.enums import ImageExtension
 
 
 class ORMImages(TimedBaseModel):
-    encoded_image = models.TextField()
-    description = models.CharField(max_length=255)
+    base64_payload = models.TextField(null=False, blank=False)
+    extension = models.CharField(
+        max_length=5,
+        choices=((ext.value, ext.value) for ext in ImageExtension),
+        default=ImageExtension.jpeg,
+    )
+    description = models.CharField(max_length=255, null=False, blank=False)
 
     def __str__(self):
         return self.description
@@ -16,7 +22,8 @@ class ORMImages(TimedBaseModel):
             id=self.id,
             created_at=self.created_at,
             updated_at=self.updated_at,
-            encoded_image=self.encoded_image,
+            base64_payload=self.base64_payload,
+            extension=self.extension,
             description=self.description,
         )
 
