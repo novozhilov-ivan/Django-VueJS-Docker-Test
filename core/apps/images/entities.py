29bcfc34168a0cef79633600446a0fc7e082ba1a
Ivan Novozhilov <petyk98@gmail.com>
2024-12-02 19:54:54 +0700

    Изменил атрибуты ImageEntity. Добавил шаблонный абстрактный метод validate в BaseEntity.

--- core/apps/images/entities.py
+++ core/apps/images/entities.py
@@ -1,15 +1,34 @@
+from abc import ABC, abstractmethod
+from base64 import b64decode
 from dataclasses import dataclass
 from datetime import datetime
 
+from core.apps.images.enums import ImageExtension
+from core.apps.images.exceptions import ValidateBase64PayloadException
 
-@dataclass
-class BaseEntity:
+
+@dataclass(kw_only=True)
+class BaseEntity(ABC):
     id: int | None = None  # noqa
     created_at: datetime | None = None
     updated_at: datetime | None = None
 
+    def __post_init__(self) -> None:
+        self.validate()
+
+    @abstractmethod
+    def validate(self) -> None:
+        raise NotImplementedError
+
 
 @dataclass(kw_only=True)
 class ImageEntity(BaseEntity):
-    encoded_image: str
+    base64_payload: str
+    extension: ImageExtension
     description: str
+
+    def validate(self) -> None:
+        try:
+            b64decode(self.base64_payload.encode(), validate=True)
+        except ValueError:
+            raise ValidateBase64PayloadException(self.base64_payload)
