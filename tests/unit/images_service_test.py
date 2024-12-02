490285e9138a5716aeafde788efb963002ed8308
Ivan Novozhilov <petyk98@gmail.com>
2024-12-02 20:02:07 +0700

    Исправил unit тесты.

--- tests/unit/images_service_test.py
+++ tests/unit/images_service_test.py
@@ -1,7 +1,7 @@
 import pytest
 
 from core.apps.images.entities import ImageEntity
-from core.apps.images.exceptions import ImageNotFound
+from core.apps.images.exceptions import ImageNotFoundException
 from core.apps.images.models import ORMImages
 from core.apps.images.services import BaseImagesService
 
@@ -21,7 +21,7 @@ def test_add_image(images_service: BaseImagesService, image_entity: ImageEntity)
 
     images_service.add_image(image_entity)
 
-    query_set = ORMImages.objects.filter(id=image_entity.id)
+    query_set = ORMImages.objects.filter(base64_payload=image_entity.base64_payload)
     assert len(query_set) == 1
 
 
@@ -62,7 +62,7 @@ def test_try_remove_non_exist_image_by_image_id(
 ):
     assert not ORMImages.objects.all()
 
-    with pytest.raises(ImageNotFound):
+    with pytest.raises(ImageNotFoundException):
         images_service.remove_image_by_id(image_entity.id)
 
 
@@ -75,7 +75,7 @@ def test_remove_exist_image_by_image_id(
 
     add_images_to_db(images_service, image_entity)
 
-    query_set = ORMImages.objects.filter(encoded_image=image_entity.encoded_image)
+    query_set = ORMImages.objects.filter(base64_payload=image_entity.base64_payload)
     assert query_set
     image_in_db: ORMImages
     image_in_db, *_ = query_set
