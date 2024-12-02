ddfc54a16c424170a6806634bcd047c78629527d
Ivan Novozhilov <petyk98@gmail.com>
2024-12-02 22:00:42 +0700

    Добавил функцию обработчик remove_image_by_id_handler. Зарегистрировал обработчик в urlpatterns. Схему RemoveImageByIdRequestSchema.

--- core/api/v1/images/remove_images/handlers.py
+++ core/api/v1/images/remove_images/handlers.py
@@ -1,9 +1,34 @@
-from rest_framework import generics
+from pydantic import ValidationError as PydanticValidationError
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.exceptions import ValidationError
+from rest_framework.request import Request
+from rest_framework.response import Response
 
-from core.api.v1.images.serializers import ImagesSerializer
+from core.api.v1.images.remove_images.schemas import RemoveImageByIdRequestSchema
+from core.apps.common.exceptions import ApplicationException
+from core.apps.images.services import BaseImagesService
+from core.project.containers import get_container
 
 
-class DeleteImageAPIView(generics.ListCreateAPIView):
-    serializer_class = ImagesSerializer
+@api_view(http_method_names=["DELETE"])
+def remove_image_by_id_handler(_: Request, id: int) -> Response:  # noqa
+    try:
+        schema = RemoveImageByIdRequestSchema(id=id)
+    except PydanticValidationError as error:
+        raise ValidationError(
+            detail=error,
+            code=status.HTTP_400_BAD_REQUEST,
+        )
 
-    # TODO получение списка изображений, добавление, удаление по id
+    container = get_container()
+    image_service: BaseImagesService = container.resolve(BaseImagesService)
+    try:
+        image_service.remove_image_by_id(image_id=schema.id)
+    except ApplicationException as exception:
+        return Response(data=exception.message, status=status.HTTP_400_BAD_REQUEST)
+
+    return Response(
+        data="Image successfully deleted",
+        status=status.HTTP_204_NO_CONTENT,
+    )
--- core/api/v1/urls.py
+++ core/api/v1/urls.py
@@ -1,26 +1,12 @@
 from django.urls import path
 
-from .images.add_image.handlers import add_image_handler
-from .images.remove_images.handlers import DeleteImageAPIView
-from core.api.v1.images.get_images.handlers import (
-    get_images_handler,
-)
+from core.api.v1.images.add_image.handlers import add_image_handler
+from core.api.v1.images.get_images.handlers import get_images_handler
+from core.api.v1.images.remove_images.handlers import remove_image_by_id_handler
 
 
 urlpatterns = [
-    path(
-        "image/",
-        add_image_handler,
-        name="add_image",
-    ),
-    path(
-        "image/",
-        DeleteImageAPIView.as_view(),
-        name="remove_image_by_id",
-    ),
-    path(
-        "images/",
-        get_images_handler,
-        name="get_images",
-    ),
+    path("image/", add_image_handler, name="add_image"),
+    path("image/<int:id>/", remove_image_by_id_handler, name="remove_image_by_id"),
+    path("images/", get_images_handler, name="get_images"),
 ]
